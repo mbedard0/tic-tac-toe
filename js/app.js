12dae903f4c5f52ad08d92ed092f4687d6185905
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-11 20:37:58 -0400

    format and center and add buttons

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
 /*-------------------------------- Constants --------------------------------*/
 
+// let win1 
+// // wins = 3 sqs in a row abs(sum) = 3
+
+// /*---------------------------- Variables (state) ----------------------------*/
+
+// let playerTurn, gameOver
+
+// /*------------------------ Cached Element References ------------------------*/
+
+// let gameStatus
+
+// const board = [
+//   '#sq0', '#sq1', '#sq2',
+//   '#sq3', '#sq4', '#sq5',
+//   '#sq6', '#sq7', '#sq8'
+// ]
 
 
-/*---------------------------- Variables (state) ----------------------------*/
+// /*----------------------------- Event Listeners -----------------------------*/
 
-
-
-/*------------------------ Cached Element References ------------------------*/
-
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-
+// const xBtn = document.getElementById('xBtn')
+// const oBtn = document.getElementById('oBtn')
+// const resetBtn = document.getElementById('resetBtn')
 
 /*-------------------------------- Functions --------------------------------*/
 
+// init()
+
+// function render() {}
+
+// if isWinner === true {
+  // return `Congrats! ${player} is the winner!`
+// } else {
+  //
+// }
+
+
+// function handleClick(){}
+
+// function init() {}
