7f33161243d627643ce322a4973db22af407705f
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-13 20:07:31 -0400

    add tie logic and message.

--- js/app.js
+++ js/app.js
@@ -34,27 +34,23 @@ function init() {
   render()
 }
 
-// gameOver = when 8 indexes do not have win condition met, or tie is declared
-
-// while gameOver !== true, run handleClick()
-
 function handleClick(evt) {
   startMsg.innerText = '';
   let idx = parseInt(evt.target.id);
   if (board[idx] === 1 || board[idx] === -1) {
     startMsg.innerText = `That square's already been picked!`
   } else {
-  board[parseInt(evt.target.id)] = playerTurn;
-  playerTurn = playerTurn * -1;
-  render()
+    board[parseInt(evt.target.id)] = playerTurn;
+    playerTurn = playerTurn * -1;
+    render()
   }
 }
 
 function render() {
   squares.forEach((square, idx) => {
-    if(board[idx] === 1) {
+    if (board[idx] === 1) {
       square.innerText = 'X';
-    } else if(board[idx] === -1) {
+    } else if (board[idx] === -1) {
       square.innerText = 'O';
     } else {
       square.innerText = ''
@@ -69,35 +65,30 @@ function render() {
 }
 
 function gameOver() {
-  if ((board[0] + board[1] + board[2] === 3) || 
-  (board[3] + board[4] + board[5] === 3) ||
-  (board[6] + board[7] + board[8] === 3) ||
-  (board[0] + board[3] + board[6] === 3) ||
-  (board[1] + board[4] + board[7] === 3) ||
-  (board[2] + board[5] + board[8] === 3) ||
-  (board[0] + board[4] + board[8] === 3) ||
-  (board[2] + board[4] + board[6] === 3)) {
+  if ((board[0] + board[1] + board[2] === 3) ||
+    (board[3] + board[4] + board[5] === 3) ||
+    (board[6] + board[7] + board[8] === 3) ||
+    (board[0] + board[3] + board[6] === 3) ||
+    (board[1] + board[4] + board[7] === 3) ||
+    (board[2] + board[5] + board[8] === 3) ||
+    (board[0] + board[4] + board[8] === 3) ||
+    (board[2] + board[4] + board[6] === 3)) {
     startMsg.innerText = 'X is the winner!';
     msgEl.innerText = 'Would you like to play again?'
-  } else if ((board[0] + board[1] + board[2] === -3) || 
-  (board[3] + board[4] + board[5] === -3) ||
-  (board[6] + board[7] + board[8] === -3) ||
-  (board[0] + board[3] + board[6] === -3) ||
-  (board[1] + board[4] + board[7] === -3) ||
-  (board[2] + board[5] + board[8] === -3) ||
-  (board[0] + board[4] + board[8] === -3) ||
-  (board[2] + board[4] + board[6] === -3)) {
-    startMsg.innerText = 'O is the winner!'
+  } else if ((board[0] + board[1] + board[2] === -3) ||
+    (board[3] + board[4] + board[5] === -3) ||
+    (board[6] + board[7] + board[8] === -3) ||
+    (board[0] + board[3] + board[6] === -3) ||
+    (board[1] + board[4] + board[7] === -3) ||
+    (board[2] + board[5] + board[8] === -3) ||
+    (board[0] + board[4] + board[8] === -3) ||
+    (board[2] + board[4] + board[6] === -3)) {
+    startMsg.innerText = 'O is the winner!';
     msgEl.innerText = 'Would you like to play again?'
-  // } else if (board.every(function(sq) {
-  //   sq = true})) {
-  //     startMsg.innerText = `There's a tie! Reset the board to start over.`;
-  //     msgEl.innerText = ``
-    }
+  } else if (board.includes(null)) {
+    return
+  } else {
+    startMsg.innerText = `It's a tie!`;
+    msgEl.innerText = 'Would you like to play again?'
+  }
 }
-
-// function absBoardSum(arr) {
-//   let sum = 0;
-//   for (let i = 0; i < arr.length; i++);
-//   sum += Math.abs(arr[i]);
-// }
